b4d797b788a240310b4f6e58148722f4ccc6dfae
margulan <dynastymm9@gmail.com>
2024-02-06 15:49:57 +0600

    Initial commit

---
+++ src/main/java/com/example/uukepkbavjtrgidmpjin/UukepkbavjtrgidmpjinApplication.java
@@ -0,0 +1,13 @@
+package com.example.uukepkbavjtrgidmpjin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UukepkbavjtrgidmpjinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UukepkbavjtrgidmpjinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uukepkbavjtrgidmpjin/UukepkbavjtrgidmpjinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uukepkbavjtrgidmpjin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UukepkbavjtrgidmpjinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
