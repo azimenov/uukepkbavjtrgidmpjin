2a91f51a636965e497b7cb159d228be45b89b84e
margulan <dynastymm9@gmail.com>
2024-04-09 09:54:14 +0500

    docker compose file added

--- src/main/java/com/example/uukepkbavjtrgidmpjin/UukepkbavjtrgidmpjinApplication.java
+++ src/main/java/com/example/uukepkbavjtrgidmpjin/UukepkbavjtrgidmpjinApplication.java
@@ -1,14 +1,7 @@
 package com.example.uukepkbavjtrgidmpjin;
 
-import com.example.uukepkbavjtrgidmpjin.customer.repository.CustomerRepository;
-import com.example.uukepkbavjtrgidmpjin.salesman.repository.SalesmanRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class UukepkbavjtrgidmpjinApplication {
