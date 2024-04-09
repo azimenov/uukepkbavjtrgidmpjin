c143c6cc083d19e69fda12f535e745784119dc82
margulan <dynastymm9@gmail.com>
2024-04-09 11:38:57 +0500

    docker compose added

--- src/main/java/com/example/uukepkbavjtrgidmpjin/customer/model/Customer.java
+++ src/main/java/com/example/uukepkbavjtrgidmpjin/customer/model/Customer.java
@@ -1,9 +1,6 @@
 package com.example.uukepkbavjtrgidmpjin.customer.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,18 +19,14 @@ public class Customer {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "customer_id")
     private int customerId;
-    @NotBlank
-    @Size(min = 5)
+
     private String username;
 
     private int year;
-    @NotBlank
-    @Pattern(regexp = "\\+7\\d{10}")
-    private String firstPhoneNumber;
-    @NotBlank
-    @Pattern(regexp = "\\+7\\d{10}")
-    private String secondPhoneNumber;
 
+    private String firstPhoneNumber;
+
+    private String secondPhoneNumber;
 
     @Column(updatable = false)
     private LocalDateTime createdAt;
